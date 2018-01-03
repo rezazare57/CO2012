358227f487879e11b7573f867bfd1affe57b09a5
rezazare57 <mreza_zare57@yahoo.com>
2018-01-03 23:21:50 +0800

    added test setup

---
+++ src/test/groovy/TaskTests.groovy
@@ -0,0 +1,14 @@
+import spock.lang.Specification
+import taskapp.application.*
+
+class TaskTests extends Specification{
+	def "Example test"() {
+		given:
+		int value1=1
+		when:
+		int value2=2
+		then:
+		value1 != value2
+
+	}
+}
